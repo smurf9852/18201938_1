9aad41433068d8886931e76eced58d81dc9cc183
Levi Kok <levi.kok@ucdconnect.ie>
2018-09-24 12:24:30 +0100

    First version of main.rb including everything

--- main.rb
+++ main.rb
@@ -10,20 +10,67 @@ class Die
 end
 
 class Point
-  # A (probably mutable) point in the grid, defined by and x- and y-coordinate.
+  attr_reader :x, :y
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+    def +(other)
+      Point.new(@x+other.x, @y+other.y)
+    end
+
+    def in_grid?(x,y)
+      puts @x+x
+      if (@x+x).between?(0,$dimension-1)
+        if (@y+y).between?(0,$dimension-1)
+          return true
+        end
+      else
+        return false
+      end
+
+
+    end
 end
+
+  # A (probably mutable) point in the grid, defined by and x- and y-coordinate.
+
 
 class Kangaroo
   # Skippy is an instance of this class, instance = object
 end
 
 class Grid
+  attr_reader :dimension
   def initialize()
-
     @dimension = inputter
     puts "dimension: #{@dimension}"
     die = Die.new
-    new_direction =  die.roll
+    position = Point.new(0,0)
+    new_direction =
+        case die.roll #[1,2,3,4]
+        when 1 then :north
+        when 2 then :east
+        when 3 then :south
+        when 4 then :west
+        end
+    puts new_direction
+    xycoordinates(new_direction)
+    puts @x_add,@y_add
+    puts position.in_grid?(@x_add,@y_add)
+    #new_direction is now north,east,south or west
+
+  end
+
+  def xycoordinates(direction)
+    x_add,y_add =
+        case direction
+        when :north then x_add=0,y_add=1
+        when :east then x_add=1,y_add=0
+        when :south then x_add=0,y_add=-1
+        when :west then x_add=-1,y_add=0
+        end
+    @x_add = x_add
+    @y_add = y_add
 
   end
 
@@ -34,9 +81,11 @@ class Grid
       puts "Wrong input!"
       inputter()
     end
-    @dimension
+    $dimension = @dimension
   end
  # The grid that Skippy is hopping about on, can be of any dimension.
 end
 
 first = Grid.new
+first.dimension
+
